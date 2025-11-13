e4327654f5cacb32243011da7b437d7d7b183cfd
Ramgopal39 <areramgopalreddy@gmail.com>
2025-11-13 22:02:19 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,9 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/income", incomeRoutes);
 app.use("/api/v1/expense", expenseRoutes);
 app.use("/api/v1/dashboard", dashboardRoutes);
+app.get("/", (req, res) => {
+    res.send("Expense Tracker API is running...");
+});
 
 //Server uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
